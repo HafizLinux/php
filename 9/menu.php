0bb84361f8cdaba718b2d8e0585b103ca1e77ebf
HafizLinux <feermanss18@gmail.com>
2023-05-07 15:14:54 +0700

    9

---
+++ 9/menu.php
@@ -0,0 +1,29 @@
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php?url=dashboard">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            
+                            
+                            <div class="sb-sidenav-menu-heading">Menu Tambahan</div>
+                            <a class="nav-link" href="index.php?url=about">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                About
+                            </a>
+                            <a class="nav-link" href="index.php?url=table">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Table
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
