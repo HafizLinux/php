6acfa99129137f843603b441f2e01193a1bd0111
HafizLinux <feermanss18@gmail.com>
2023-05-13 06:50:47 +0700

    12

--- 12/admin/top.php
+++ 12/admin/top.php
@@ -4,9 +4,6 @@
         echo "<script>document.location.href = 'login.php'</script>";
 		exit;
 	}
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,14 +13,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Static Navigation - SB Admin</title>
+        <title>Admin Senggol</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand ps-3" href="">PT Senggol Dong</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
